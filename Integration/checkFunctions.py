5dbbbaf454a11af7aec92fb2d8e483caa37021b1
Thilaga2001 <thilagavedaraju@gmail.com>
2024-06-12 16:17:38 -0400

    Assignment 2 code pushed

---
+++ Integration/checkFunctions.py
@@ -0,0 +1,13 @@
+
+
+def oddOrEven(num1):
+    if (num1 % 2) == 0:
+       return "even"
+    else:
+        return "odd"
+
+def checkNegative(num1):
+    if num1 > 0:
+        return "positive"
+    else:
+        return "negative"
